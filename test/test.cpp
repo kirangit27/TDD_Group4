61042cb3c5d80f598bd74b064e3841a23070a922
kirangit27 <kiran.suvas.patil@gmail.com>
2023-10-10 12:55:36 -0400

    modified test cases

--- test/test.cpp
+++ test/test.cpp
@@ -14,8 +14,8 @@ TEST(testPID_1, should_pass) {
 // Additional Test cases
 
 TEST(testPID_2, should_pass) {
-  PID pid(1.0, 0.0, 0.5, 0.1);
-  EXPECT_NEAR(pid.compute(15.0, 10.0), 40.0, 2.00);
+  PID pid(0.1, 0.0, 0.1, 0.1);
+  EXPECT_NEAR(pid.compute(15.0, 10.0), 15.5, 2.00);
 }
 
 /*
@@ -24,14 +24,14 @@ error = ref_vel - vel
 error = 15.0 - 10.0 = 5.0
 error_i = 0 (for simplicity, as we don't have preceeding values)
 new_vel = vel + Kp*error + Ki*(error*dt) + Kd*(error - error_i)/dt
-new_vel = 10 + 5 + 0 + 0.5*5*10 
-new_vel = 40
+new_vel = 10 + 0.5 + 0 + (0.1*5)/0.1 
+new_vel = 15,5
 */
 
 
 TEST(testPID_3, should_pass) {
-  PID pid(0.5, 2.0, 0.0, 0.1);
-  EXPECT_NEAR(pid.compute(5.0, 6.0), 9.8, 1.00);
+  PID pid(0.1, 0.0, 0.1, 0.1);
+  EXPECT_NEAR(pid.compute(5.0, 6.0), 4.9, 1.00);
 }
 
 /*
@@ -40,7 +40,7 @@ error = ref_vel - vel
 error = 5.0 - 6.0 = -1.0
 error_i = 0 (for simplicity, as we don't have preceeding values)
 new_vel = vel + Kp*error + Ki*(error*dt) + Kd*(error - error_i)/dt
-new_vel = 6 - 0.5 + 2.0*(-1*0.1) + 0
-new_vel = 6 - 0.5 - 0.2
-new_vel = 5.3
+new_vel = 6 - 0.1 + 0.0*(-1*0.1) + 0.1(-1.0-0.0)/0.1
+new_vel = 6 - 0.1 - 1.0
+new_vel = 4.9
 */
